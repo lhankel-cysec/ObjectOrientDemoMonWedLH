638d88aa9d99c08eefc14a6c015a098747e2f594
Leland <leland.hankel@gmail.com>
2021-09-22 15:55:50 -0400

    object orient demo sep 22

--- BankAccount.java
+++ BankAccount.java
@@ -3,10 +3,22 @@ package com.company;
 public class BankAccount {
     private double balance;
     private float interestRate;
+    private int accountID;
+    private static int nextID = 1000;
+
 
     public BankAccount(){
         interestRate = 0.02f;
+        accountID = nextID;
+        nextID++;
     }
+    public BankAccount(double initialBalance, float initialRate){
+        balance = initialBalance;
+        interestRate = initialRate;
+        accountID = nextID;
+        nextID += 1;
+    }
+
     public void deposit(double amount){
         balance = balance + amount;
     }
@@ -25,5 +37,8 @@ public class BankAccount {
     }
     public void printBalance(){
         System.out.println(balance);
+    }
+    public int getAccountID(){
+        return accountID;
     }
 }
